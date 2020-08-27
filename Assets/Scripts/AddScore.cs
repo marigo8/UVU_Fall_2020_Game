485b5991ec427eca4de4a0a45305a31858bdeef4
Mario Tijerino <mariogotijerino@gmail.com>
2020-08-26 18:23:08 -0600

    Learned about Scriptable Objects

---
+++ Assets/Scripts/AddScore.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddScore : MonoBehaviour
+{
+    public IntData scoreValue;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        scoreValue.value++;
+    }
+}
