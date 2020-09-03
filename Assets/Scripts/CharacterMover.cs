4fcac70a9bc75be5feea3cca48c70b030ed35600
Mario Tijerino <mariogotijerino@gmail.com>
2020-09-03 13:05:37 -0600

    Added control style toggle
    pressing the V key toggles between vehicle style movement and character style movement

--- Assets/Scripts/CharacterMover.cs
+++ Assets/Scripts/CharacterMover.cs
@@ -1,35 +1,45 @@
 ﻿using UnityEngine;
+using UnityEngine.Serialization;
 
 [RequireComponent(typeof(CharacterController))]
 public class CharacterMover : MonoBehaviour
 {
     private CharacterController controller;
+    
     private Vector3 movement;
+
+    [FormerlySerializedAs("useCarStyle")] public bool useVehicleStyle;
 
     public float moveSpeed = 5f, sprintModifier = 2f, rotateSpeed = 30f, gravity = -9.81f, jumpForce = 30f;
     private float yVar;
 
     public int jumpCountMax = 2;
     private int jumpCount;
+
     private void Start()
     {
         controller = GetComponent<CharacterController>();
     }
-    
+
     private void Update()
     {
-        print(controller.velocity.y);
-        var vInput = Input.GetAxis("Vertical") * moveSpeed;
-        if (Input.GetButton("Sprint"))
+        if (Input.GetKeyDown(KeyCode.V))
         {
-            vInput *= sprintModifier;
+            useVehicleStyle = !useVehicleStyle;
+            transform.SetPositionAndRotation(transform.position, Quaternion.identity);
         }
-        movement.Set(vInput, yVar, 0);
+        
+        if (useVehicleStyle)
+        {
+            MoveVehicleStyle();
+            movement = transform.TransformDirection(movement);
+        }
+        else
+        {
+            MoveNormalStyle();
+        }
 
-        var hInput = Input.GetAxis("Horizontal") * rotateSpeed * Time.deltaTime;
-        transform.Rotate(0,hInput,0);
-
-        yVar += gravity*Time.deltaTime;
+        yVar += gravity * Time.deltaTime;
 
         if (controller.isGrounded && movement.y <= 0)
         {
@@ -43,9 +53,36 @@ public class CharacterMover : MonoBehaviour
             jumpCount++;
         }
 
-        
 
-        movement = transform.TransformDirection(movement * Time.deltaTime);
-        controller.Move(movement);
+
+        controller.Move(movement * Time.deltaTime);
     }
+
+    private void MoveVehicleStyle()
+    {
+        var vInput = Input.GetAxis("Vertical") * moveSpeed;
+        if (Input.GetButton("Sprint"))
+        {
+            vInput *= sprintModifier;
+        }
+
+        movement.Set(0, yVar, vInput);
+
+        var hInput = Input.GetAxis("Horizontal") * rotateSpeed * Time.deltaTime;
+        transform.Rotate(0, hInput, 0);
+    }
+
+    private void MoveNormalStyle()
+    {
+        var hInput = Input.GetAxis("Horizontal");
+        var vInput = Input.GetAxis("Vertical");
+        movement.Set(hInput, 0, vInput);
+        movement = Vector3.ClampMagnitude(movement, 1f) * moveSpeed;
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            movement *= sprintModifier;
+        }
+        movement.y = yVar;
+    }
+
 }
