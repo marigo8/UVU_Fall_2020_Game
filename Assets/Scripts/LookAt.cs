3ffe243ecf174774fa25e27287090224e999bf82
Mario Tijerino <mariogotijerino@gmail.com>
2020-09-16 14:23:35 -0600

    Did Lerping, LookAt, and MoveLocator in class

---
+++ Assets/Scripts/LookAt.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAt : MonoBehaviour
+{
+    public Transform lookAtObj;
+    private void Update()
+    {
+        var lookAt = Vector3.zero;
+        var rot = transform.eulerAngles;
+        rot.x = 0;
+        transform.rotation = Quaternion.Euler(rot);
+    }
+}
---
+++ Assets/Scripts/MoveLocator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveLocator : MonoBehaviour
+{
+    private Camera cam;
+    public Transform pointObj;
+
+    private void Start()
+    {
+        cam = Camera.main;
+    }
+
+    private void OnMouseDown()
+    {
+        if (Physics.Raycast(cam.ScreenPointToRay(Input.mousePosition), out var hit, 100))
+        {
+            pointObj.position = hit.point;
+        }
+    }
+}
