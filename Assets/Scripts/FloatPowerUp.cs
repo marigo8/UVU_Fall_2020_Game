459d4173f1e6cd68a39af4df57f01097506437f6
Mario Tijerino <mariogotijerino@gmail.com>
2020-09-19 12:57:21 -0600

    replaced JumpPowerUp with more universal scripts. made power ups respawn

---
+++ Assets/Scripts/FloatPowerUp.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class FloatPowerUp : MonoBehaviour
+{
+    public FloatData playerFloatData, defaultFloatData, newFloatData;
+    [SerializeField] private float buffTime = 2f, respawnTime;
+    [SerializeField] private bool respawn;
+
+    private void Start()
+    {
+        playerFloatData.value = defaultFloatData.value;
+    }
+
+    private IEnumerator OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<CharacterMover>() == null) yield break;
+        
+        playerFloatData.value = newFloatData.value;
+        
+        GetComponent<MeshRenderer>().enabled = false;
+        GetComponent<Collider>().enabled = false;
+        
+        yield return new WaitForSeconds(buffTime);
+        
+        playerFloatData.value = defaultFloatData.value;
+        
+        if (respawn)
+        {
+            yield return new WaitForSeconds(respawnTime);
+            GetComponent<MeshRenderer>().enabled = true;
+            GetComponent<Collider>().enabled = true;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
