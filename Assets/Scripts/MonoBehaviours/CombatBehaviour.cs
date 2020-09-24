9c3b83bbe5681f261ac9b04e39cd56a4b5c23265
Mario Tijerino <mariogotijerino@gmail.com>
2020-09-23 18:27:10 -0600

    Created TriggerBehaviour and CombatBehaviour

---
+++ Assets/Scripts/MonoBehaviours/CombatBehaviour.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CombatBehaviour : MonoBehaviour
+{
+    public void DealDamage(Collider target)
+    {
+        print(target.name);
+    }
+}
---
+++ Assets/Scripts/MonoBehaviours/TriggerBehaviour.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+[System.Serializable]
+public class TriggerEventClass : UnityEvent<Collider> {}
+
+public class TriggerBehaviour : MonoBehaviour
+{
+    public TriggerEventClass triggerEnterEvent;
+    public TriggerEventClass triggerStayEvent;
+    public TriggerEventClass triggerExitEvent;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        triggerEnterEvent.Invoke(other);
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        triggerStayEvent.Invoke(other);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        triggerExitEvent.Invoke(other);
+    }
+}
