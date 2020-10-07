42148aeba06f56cde729e6706feb380aa3a28ad9
Mario Tijerino <mariogotijerino@gmail.com>
2020-10-07 14:30:08 -0600

    Class: AI and Coroutines

---
+++ Assets/Scripts/MonoBehaviours/AIBehaviour.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class AIBehaviour : MonoBehaviour
+{
+    // !!! WARNING: UGLY SCRIPT !!! //
+    private NavMeshAgent agent;
+    public Transform player;
+    private bool canNavigate = true;
+    private WaitForFixedUpdate wffu;
+    public float holdTime = 1f;
+    private WaitForSeconds wfs;
+
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        wfs = new WaitForSeconds(holdTime);
+    }
+
+    private IEnumerator Navigate()
+    {
+        
+        canNavigate = true;
+        yield return wfs;
+        while (canNavigate)
+        {
+            yield return wffu;
+            agent.destination = player.position;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        canNavigate = false;
+        StartCoroutine(Navigate());
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        canNavigate = false;
+    }
+}
