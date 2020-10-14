606b8040c8fdbf9fe15511d3cee200cda6d42ab3
Mario Tijerino <mariogotijerino@gmail.com>
2020-10-14 00:22:02 -0600

    Started work on enemies consuming distractions.

---
+++ Assets/Scripts/MonoBehaviours/DistractionBehaviour.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DistractionBehaviour : MonoBehaviour
+{
+    private Rigidbody rb;
+    
+    public Vector3 force;
+    public float life;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        var forceDirection = force;
+        rb.AddRelativeForce(forceDirection);
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            life -= Time.fixedDeltaTime;
+            Debug.Log(Time.time);
+
+            if (life <= 0)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
