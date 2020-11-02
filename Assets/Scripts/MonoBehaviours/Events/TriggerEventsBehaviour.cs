c212d3ec2d4733c1e4d86d723a962c15e7fe7142
Mario Tijerino <mariogotijerino@gmail.com>
2020-11-01 17:02:15 -0700

    Changed ColliderEventBehaviour to TriggerEventsBehaviour.

---
+++ Assets/Scripts/MonoBehaviours/Events/TriggerEventsBehaviour.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(Collider))]
+public class TriggerEventsBehaviour : MonoBehaviour
+{
+    public string filterTag;
+    
+    public UnityEvent<Collider> triggerEnterEvent;
+    public UnityEvent<Collider> triggerStayEvent;
+    public UnityEvent<Collider> triggerExitEvent;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (filterTag != "") // If there is no tag, just invoke the event.
+        {
+            if (!other.CompareTag(filterTag)) return; // if there is a tag but it doesn't match, don't invoke the event.
+        }
+        triggerEnterEvent.Invoke(other);
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (filterTag != "")
+        {
+            if (!other.CompareTag(filterTag)) return;
+        }
+        triggerStayEvent.Invoke(other);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (filterTag != "")
+        {
+            if (!other.CompareTag(filterTag)) return;
+        }
+        triggerExitEvent.Invoke(other);
+    }
+}
