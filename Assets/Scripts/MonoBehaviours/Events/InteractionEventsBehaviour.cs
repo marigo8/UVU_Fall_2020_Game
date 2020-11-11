2db99794ae942437c2710b465757a1746d2e952c
Mario Tijerino <mariogotijerino@gmail.com>
2020-11-11 11:39:17 -0700

    Interaction

---
+++ Assets/Scripts/MonoBehaviours/Events/InteractionEventsBehaviour.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(Collider))]
+public class InteractionEventsBehaviour : MonoBehaviour
+{
+    public bool toggle;
+    
+    public UnityEvent onInteractionEvent, offInteractionEvent;
+
+    private Collider col;
+    private bool ready, toggleOn = true;
+
+    private void Start()
+    {
+        col = GetComponent<Collider>();
+    }
+
+    private void Update()
+    {
+        if (!ready) return;
+
+        if (Input.GetButtonDown("Interact"))
+        {
+            if (toggleOn)
+            {
+                onInteractionEvent.Invoke();
+            }
+            else
+            {
+                offInteractionEvent.Invoke();
+            }
+
+            if (toggle)
+            {
+                toggleOn = !toggleOn;
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!other.CompareTag("Player")) return;
+        ready = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (!other.CompareTag("Player")) return;
+        ready = false;
+    }
+}
