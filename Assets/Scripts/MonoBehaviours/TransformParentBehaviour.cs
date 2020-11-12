87b7577206a64ea9fb9d71cda5b3fb4dcc6c99a1
Mario Tijerino <mariogotijerino@gmail.com>
2020-11-11 20:28:45 -0700

    Platforms use GameActions. Got rid of LerpBehaviour

---
+++ Assets/Scripts/MonoBehaviours/TransformParentBehaviour.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class TransformParentBehaviour : MonoBehaviour
+{
+    public void SetColliderParent(Collider other)
+    {
+        other.transform.parent = transform;
+    }
+
+    public void ClearColliderParent(Collider other)
+    {
+        other.transform.parent = null;
+    }
+}
