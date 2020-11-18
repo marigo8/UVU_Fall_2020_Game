fc6fe97be974f7bf07e84734a2677c52a18a9c4e
Mario Tijerino <mariogotijerino@gmail.com>
2020-11-17 21:52:54 -0700

    Substance In Unity

---
+++ Assets/Scripts/MonoBehaviours/SubstanceBehaviour.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using Substance.Game;
+
+public class SubstanceBehaviour : MonoBehaviour
+{
+    public SubstanceGraph graph;
+    public int scratchSpeed = 500;
+
+    public void AddScratches()
+    {
+        var scratches = graph.GetInputInt("Metal_Scratches_Amount");
+        scratches += (int) (scratchSpeed * Time.fixedDeltaTime);
+        graph.SetInputInt("Metal_Scratches_Amount", scratches);
+        graph.QueueForRender();
+        Substance.Game.Substance.RenderSubstancesAsync();
+    }
+}
