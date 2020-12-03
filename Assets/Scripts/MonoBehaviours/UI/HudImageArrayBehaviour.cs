5f77441b05c807878155d6e490d216baa146cd38
Mario Tijerino <mariogotijerino@gmail.com>
2020-12-03 00:04:44 -0700

    Health Hud that uses multiple images

---
+++ Assets/Scripts/MonoBehaviours/UI/HudImageArrayBehaviour.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(RectTransform))]
+public class HudImageArrayBehaviour : MonoBehaviour
+{
+    public Image imagePrefab;
+    public IntData data;
+
+    private RectTransform rectTransform;
+
+    private List<Image> images;
+
+    private void Start()
+    {
+        rectTransform = GetComponent<RectTransform>();
+        images = new List<Image>();
+
+        var imageWidth = imagePrefab.rectTransform.sizeDelta.x;
+        var offset = rectTransform.position + Vector3.right * imageWidth / 2;
+        for (var i = 0; i < data.maxValue; i++)
+        {
+            var image = Instantiate(imagePrefab, transform);
+            images.Add(image);
+            
+            var imageRect = image.GetComponent<RectTransform>();
+            if(imageRect == null) throw new Exception("Didn't work");
+
+            imageRect.position = offset;
+            
+            offset += Vector3.right * imageWidth;
+        }
+    }
+
+    public void UpdateHud()
+    {
+        for (var i = 0; i < data.maxValue; i++)
+        {
+            images[i].enabled = i < data.value;
+        }
+    }
+}
